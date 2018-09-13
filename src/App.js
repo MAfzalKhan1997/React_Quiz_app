93799fff2b6112126ec9927c32fd11d67f35ce96
Muhammad Afzal Khan <MAfzalKhan1997@gmail.com>
2018-09-13 15:38:31 +0400

    quiz app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import QuizList from './screens/QuizList/QuizList'
+import QuizInfo from './screens/QuizInfo/QuizInfo'
+import StartQuiz from './screens/StartQuiz/StartQuiz'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor() {
+    super()
+    
+    this.state = {
+      quizzes: [
+        {name: 'AngularJs', 
+        subQuiz: [ {name:'Quiz 1',questions:'50',time:'60'},{name:'Quiz 2',questions:'40',time:'40'} ] 
+        },
+        {name: 'React', 
+        subQuiz: [ {name:'Quiz 1',questions:'30',time:'60'},{name:'Quiz 2',questions:'25',time:'50'} ] 
+        },
+        {name: 'PWA', 
+        subQuiz: [ {name:'Quiz 1',questions:'100',time:'120'} ] 
+        },
+        {name: 'JavaScript',
+        subQuiz: [ {name:'Quiz 1',questions:'30',time:'40'},{name:'Quiz 2',questions:'35',time:'60'} ]
+        }
+      ]
+    };
+
+    this.joinQuiz = this.joinQuiz.bind(this);
+    this.startQuiz = this.startQuiz.bind(this);
+    this.showList = this.showList.bind(this);
   }
+
+  joinQuiz(index) {
+    const {quizzes} = this.state;
+    this.setState({quiz: quizzes[index]});
+  }
+
+  startQuiz(index){
+    const {quizzes} = this.state;
+    console.log(index)
+    this.setState({started: quizzes[index]});
+  }
+
+  showList() {
+    this.setState({quiz: null});
+  }
+
+  render(){
+    const {quizzes, quiz} = this.state;
+
+    return(
+      <div>
+        {!quiz && !started && <QuizList list={quizzes} onPress={this.joinQuiz}/>}
+        {quiz && !started && <QuizInfo quiz={quiz} onPress={this.startQuiz} onBack={this.showList}/>}
+        {started && <StartQuiz />}
+      </div>
+      )
+    }
+    
 }
 
 export default App;
---
+++ src/screens/QuizInfo/QuizInfo.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class QuizInfo extends Component {
+  constructor() {
+    super()
+  }
+
+  render(){
+    const {quiz, onBack ,onPress} = this.props;
+    return(
+      <div>
+        {console.log(quiz) }
+        <h1>{quiz.name}</h1>
+        <ul>
+          {quiz.subQuiz.map((subQuiz,index)=>{
+            return <li>
+              {<h3>{subQuiz.name}</h3>}
+              <p>Total Questions: {subQuiz.questions}</p>
+              <p>Total Time: {subQuiz.time}</p>
+              <button onClick={() => onPress(index)}>Start Quiz</button>
+              <button onClick={onBack}>Back</button>
+              </li>
+          })}
+          </ul>
+ 
+      </div>
+      )
+    }
+    
+}
+
+export default QuizInfo;
+
+//CHILD --> PARENT STATE UPDATE
+//=============================
+
+//1) Create a function in Parent that
+//will update the State.
+
+//2) Pass the function in the Child's
+//Component's Props
+
+//3) Call that function from Child Props.
